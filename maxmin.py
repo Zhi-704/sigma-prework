0afa0004d856a7d1409ff432f1d326c92234bdef
zll19 <zll19@ic.ac.uk>
2024-02-02 16:22:27 +0000

    Trying more git stuff

--- maxmin.py
+++ maxmin.py
@@ -1,24 +1,29 @@
 '''
 When given a date, calculates the age between the current dat and the given date.
 '''
+'''
+Testing out Git Stuff
+'''
+
 
 def main():
-  test_list = [20,50,12,6,0,8]
-  maxmin(test_list)
-  
+    test_list = [20, 50, 12, 6, 0, 8]
+    maxmin(test_list)
+
 
 def maxmin(input_list):
-  if len(input_list) == 0:
-    print(input_list)
-    return
-  max = input_list[0]
-  min = input_list[0]
-  for element in input_list:
-    if element < min:
-      min = element
-    if element > max:
-      max = element
-  print([min,max])
+    if len(input_list) == 0:
+        print(input_list)
+        return
+    max = input_list[0]
+    min = input_list[0]
+    for element in input_list:
+        if element < min:
+            min = element
+        if element > max:
+            max = element
+    print([min, max])
+
 
 if __name__ == '__main__':
-  main()
+    main()
